23fea768bfe06e0d0335034df6584493d9eeaf77
phantichchai <phantichchai1998@gmail.com>
2020-09-08 20:53:56 +0700

    new branch develop

--- src/features/Login/loginScreen.js
+++ src/features/Login/loginScreen.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 export default function LoginScreen( {navigation} ) {
     return (
         <View>
-            <Text>LoginScreen</Text>
+            <Text>LoginScreen by fast</Text>
         </View>
     );
 }
